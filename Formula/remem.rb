89b88966c476a364a5716997ab4e345d094d3d97
lif <1835304752@qq.com>
2026-06-13 01:56:14 +0800

    remem 0.5.11 (#5)

--- Formula/remem.rb
+++ Formula/remem.rb
@@ -4,28 +4,28 @@
 class Remem < Formula
   desc "Persistent memory for Claude Code and Codex"
   homepage "https://github.com/majiayu000/remem"
-  version "0.4.5"
+  version "0.5.11"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/majiayu000/remem/releases/download/v0.4.5/remem-darwin-x64.tar.gz"
-      sha256 "71d13ddd4935dd9e13e37c8ec1ff081934fb7a12c1d83c6c7b9b425a7acaab4d"
+      url "https://github.com/majiayu000/remem/releases/download/v0.5.11/remem-darwin-x64.tar.gz"
+      sha256 "8f500efddacf9db65cd882ecce9230f037a0fef9f2af46a3e3f7ea1917901eb0"
     end
     on_arm do
-      url "https://github.com/majiayu000/remem/releases/download/v0.4.5/remem-darwin-arm64.tar.gz"
-      sha256 "35ffef27827c66e96c60149524c20e3af572c75f8f5d597eb740906f97255c22"
+      url "https://github.com/majiayu000/remem/releases/download/v0.5.11/remem-darwin-arm64.tar.gz"
+      sha256 "7d2b53decaa1c37c11d9c18aa72c9e9acb8f92e18f88db9b2f534147f0fbf794"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/majiayu000/remem/releases/download/v0.4.5/remem-linux-x64.tar.gz"
-      sha256 "91106ddecc684ab223f343caf089312ca61a39c42e323ea5c6ea57fb82e5100b"
+      url "https://github.com/majiayu000/remem/releases/download/v0.5.11/remem-linux-x64.tar.gz"
+      sha256 "680062a55b539c697567bb51dca5e21c08ca6a6e3aa11ff94d1e84dabb924a55"
     end
     on_arm do
-      url "https://github.com/majiayu000/remem/releases/download/v0.4.5/remem-linux-arm64.tar.gz"
-      sha256 "27ac660646801aa14d89cfcab4fc626d6dbb7992dfc43a4e9000fbd971a4dc61"
+      url "https://github.com/majiayu000/remem/releases/download/v0.5.11/remem-linux-arm64.tar.gz"
+      sha256 "3eb53700c2bb028f4c2a829e1ee6a7bb56e1252e3e2166154c569ed8cae1598c"
     end
   end
 
@@ -34,6 +34,6 @@ class Remem < Formula
   end
 
   test do
-    assert_match "remem 0.4.5", shell_output("#{bin}/remem --version")
+    assert_match "remem 0.5.11", shell_output("#{bin}/remem --version")
   end
 end
