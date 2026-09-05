3af1e02b6dcd2e428905775d31f5e6b6462b9c31
lif <1835304752@qq.com>
2026-09-06 03:47:38 +0800

    ccstats 0.7.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ccstats.rb
+++ Formula/ccstats.rb
@@ -1,8 +1,8 @@
 class Ccstats < Formula
   desc "Fast Claude Code and OpenAI Codex usage statistics CLI"
   homepage "https://github.com/majiayu000/ccstats"
-  url "https://static.crates.io/crates/ccstats/ccstats-0.7.0.crate"
-  sha256 "9ed30032ff0324206a436da7b0afc7de2cb5d8bce4e398f4d254ebc28b7ac5d7"
+  url "https://static.crates.io/crates/ccstats/ccstats-0.7.1.crate"
+  sha256 "7957c8bb65e2ef6c2c7e92ac49b5104932d6a2f81c8073fd7c9c27666b52c0b8"
   license "MIT"
 
   depends_on "rust" => :build
